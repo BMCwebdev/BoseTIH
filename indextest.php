b537d3498df04591bd1454c1e0718f0086b18d7b
Brian <Brian@bmcwebdev.com>
2014-11-02 15:44:29 -0500

    ..

--- indextest.php
+++ indextest.php
@@ -263,12 +263,6 @@
 							</table>
 							
 							<div class="captchadiv">
-							
-							<script type="text/javascript">
-var RecaptchaOptions = {
-   lang : 'fr',
-};
-</script>
 								<?php
 								
 								require_once('recaptchalib.php');
