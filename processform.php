9f665133fd46d42ad77f5b17674e3fe9277e3f27
Chad Nale <chad.nale@gmail.com>
2014-11-02 11:23:06 -0500

    added email functionality

--- processform.php
+++ processform.php
@@ -3,6 +3,17 @@
 // chad nale chad.nale@gmail.com
 
 require "site.inc";
+
+
+
+$isFrench = false;
+if(0 < strpos($_SERVER["HTTP_REFERER"], "indexFR.html"))
+{
+    $isFrench = true;
+}
+
+print("isFrench=" . $isFrench);
+die();
 // this is the processing page for the sweepstake form
  $result = storeEnrollmentForm();
  if($result == "Email exists already...")
@@ -12,6 +23,9 @@ require "site.inc";
  print("completed....<br><hr>");
  // now print out the data so we can see what went in
  getJSONData();
+ 
+ 
+ // here we email out the correct email based upon where it came from...
  print("<br><hr>");
 
 
