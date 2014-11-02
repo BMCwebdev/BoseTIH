33427c592ded358fae7cf165e6c6f708c857cf81
Brian <Brian@bmcwebdev.com>
2014-11-02 15:25:16 -0500

    added captcha to working index

--- indextest.php
+++ indextest.php
@@ -259,38 +259,38 @@
 							
 							
 							<div class="captchadiv">
-<?php
+								<?php
+								
+								require_once('recaptchalib.php');
+								
+								// Get a key from https://www.google.com/recaptcha/admin/create
+								$publickey = "6Lcx4PwSAAAAAHa6T8y4MMurRFzEy2PM4gvMM9OS";
+								$privatekey = "6Lcx4PwSAAAAAHF4RSSRpGRvuZ6OroYN-NEFnNHS";
+								
+								# the response from reCAPTCHA
+								$resp = null;
+								# the error code from reCAPTCHA, if any
+								$error = null;
+								
+								# was there a reCAPTCHA response?
+								if ($_POST["recaptcha_response_field"]) {
+								        $resp = recaptcha_check_answer ($privatekey,
+								                                        $_SERVER["REMOTE_ADDR"],
+								                                        $_POST["recaptcha_challenge_field"],
+								                                        $_POST["recaptcha_response_field"]);
+								
+								        if ($resp->is_valid) {
+								                echo "You got it!";
+								        } else {
+								                # set the error code so that we can display it
+								                $error = $resp->error;
+								        }
+								}
+								echo recaptcha_get_html($publickey, $error);
+								?>
 
-require_once('recaptchalib.php');
-
-// Get a key from https://www.google.com/recaptcha/admin/create
-$publickey = "6Lcx4PwSAAAAAHa6T8y4MMurRFzEy2PM4gvMM9OS";
-$privatekey = "6Lcx4PwSAAAAAHF4RSSRpGRvuZ6OroYN-NEFnNHS";
-
-# the response from reCAPTCHA
-$resp = null;
-# the error code from reCAPTCHA, if any
-$error = null;
-
-# was there a reCAPTCHA response?
-if ($_POST["recaptcha_response_field"]) {
-        $resp = recaptcha_check_answer ($privatekey,
-                                        $_SERVER["REMOTE_ADDR"],
-                                        $_POST["recaptcha_challenge_field"],
-                                        $_POST["recaptcha_response_field"]);
-
-        if ($resp->is_valid) {
-                echo "You got it!";
-        } else {
-                # set the error code so that we can display it
-                $error = $resp->error;
-        }
-}
-echo recaptcha_get_html($publickey, $error);
-?>
-
-<input type="button" id="btnsubmit" class="btn btn-primary" name="btnsubmit" onclick="javascript:processForm();" value="Submit">
-</div>
+								<input type="button" id="btnsubmit" class="btn btn-primary" name="btnsubmit" onclick="javascript:processForm();" value="Submit">
+							</div>
 
 
 
