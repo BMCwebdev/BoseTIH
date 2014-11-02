3c6123ced4676d5916a9c2a6d4c490deb3ddea15
Chad Nale <chad.nale@gmail.com>
2014-11-02 11:24:31 -0500

    new processform

--- processform.php
+++ processform.php
@@ -3,17 +3,13 @@
 // chad nale chad.nale@gmail.com
 
 require "site.inc";
-
-
-
+// determine if this is french....
 $isFrench = false;
 if(0 < strpos($_SERVER["HTTP_REFERER"], "indexFR.html"))
 {
     $isFrench = true;
 }
 
-print("isFrench=" . $isFrench);
-die();
 // this is the processing page for the sweepstake form
  $result = storeEnrollmentForm();
  if($result == "Email exists already...")
@@ -23,7 +19,7 @@ die();
  print("completed....<br><hr>");
  // now print out the data so we can see what went in
  getJSONData();
- 
+ // here we need to send the email...
  
  // here we email out the correct email based upon where it came from...
  print("<br><hr>");
