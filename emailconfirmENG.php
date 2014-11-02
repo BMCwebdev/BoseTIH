b2fa3e5ef4c5940bc52c3dbffe8fc62c118e529d
Brian <Brian@bmcwebdev.com>
2014-11-02 00:02:53 -0400

    test validator

--- emailconfirmENG.php
+++ emailconfirmENG.php
@@ -1,35 +1,48 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<?php
+$to = "!!!!!!!!!!!!!!!!!";
+$subject = "Bose &ldquo;Bring It Home!&rdquo; Program – Enrollment Confirmation";
 
+$message = "
 <html>
 <head>
     <title>Bose &ldquo;Bring It Home!&rdquo; Program – Enrollment Confirmation</title>
 </head>
 
 <body>
-<div style="max-width:940px;margin-left:auto;margin-right:auto;">
 <div>
 <img src="/assets/images/hero.jpg" alt="hero" style="width:100%;" />
 </div>
 
     <p>Bose and its promotion partner Promosis, Inc. sent you this message. To ensure delivery to your inbox (not bulk or spam folders), please add <a href="mailto:bringithome@email-hub.com">bringithome@email-hub.com</a> to your address book.</p>
 
-    <p>Dear [@@@@@@@@@@@@@@@@first name@@@@@@@@@@@@@@@@],<br />
+    <p>Dear [first name],<br />
     <br />
     Thanks for enrolling in the Bose&#174; &ldquo;<em>Bring It Home!&rdquo;</em> SoundTouch&#8482; systems sales program.</p>
 
-    <p>Log-in to the site to submit each of your sales of a qualifying SoundTouch(&#8482;) system<br />
+    <p>Log-in to the site to submit each of your sales of a qualifying SoundTouch(TM) system<br />
     <br />
     If you qualify for a prize, it will be shipped to the address supplied during program enrollment. If your shipping address has changed since you enrolled, you must let us know the new address.</p>
 
    <div>**********</div><br />
-    <div>Your enrolled E-mail address is: [@@@@@@@@@@@@@@email address@@@@@@@@@@@@@@@@]</div><br />
+    <div>Your enrolled E-mail address is: [email address]</div><br />
     <div>***********</div>
     <br />
     Thanks again for joining our promotion, and good luck!<br />
     <br />
-    [@@@@@@@@@@@@@@rules.html@@@@@@@@@@@@@@@@@]<br />
+    [Official Rules link]<br />
     <br />
     Click here to be removed from our email list.</p>
-</div>
 </body>
 </html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <bringithome@email-hub.com>' . "\r\n";
+/* $headers .= 'Cc: someone@someplace.com' . "\r\n"; */
+
+mail($to,$subject,$message,$headers);
+?>
---
+++ emailconfirmFR.php
@@ -0,0 +1,49 @@
+<?php
+$to = "!!!!!!!!!!!!!!!!!";
+$subject = "Bose &ldquo;Bring It Home!&rdquo; Program – Enrollment Confirmation";
+
+$message = "
+<html>
+<head>
+    <title>Programme &#171; Gagnez-moi &#187; de Bose &ndash; confirmation de votre inscription</title>
+</head>
+
+<body>
+<div>
+<img src="/assets/images/hero.jpg" alt="hero" style="width:100%;" />
+</div>
+
+    <p>Bose et leur partenaire promotionnel Promosis, Inc. vous adressent ce message.  Pour assurer la réception de nos messages dans votre messagerie (non pas votre dossier ordure ou dossier en bloc), merci d’ajouter l’adresse suivante à votre carnet d’adresses <a href="mailto:bringithome@email-hub.com">bringithome@email-hub.com</a>.</p>
+
+    <p>Cher/Chère [@@@@@@@@@@@@@@@@first name@@@@@@@@@@@@@@@@],<br />
+    <br />
+    Merci de vous inscrire au programme de ventes &#171; Gagnez-moi &#187;  qui vous donne la chance de remporter un système de musique SoundTouch&#8482; de Bose&#174;.</p>
+
+    <p>Identifiez-vous sur le site pour soumettre chacune de vos ventes d'un système SoundTouch&#8482; admissible.<br />
+    <br />
+    Si vous vous qualifiez pour un prix, celui-ci vous parviendra à l’adresse que vous avez fournie au moment de votre inscription au programme.  Si votre adresse d’expédition a été modifiée depuis votre inscription, veuillez nous en aviser.</p>
+
+   <div>**********</div><br />
+    <div>Votre adresse courriel d’inscription est la suivante:  [@@@@@@@@@@@@@@email address@@@@@@@@@@@@@@@@]</div><br />
+    <div>***********</div>
+    <br />
+    Encore une fois, merci de prendre part au programme et bonne chance!<br />
+    <br />
+    [@@@@@@@@@@@@@@rules.html@@@@@@@@@@@@@@@@@]<br />
+    <br />
+    <a href="@@@@@@@@@@@@@@@@@">Cliquez ici</a> pour être retiré de notre liste d’envoi.</p>
+</div>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <bringithome@email-hub.com>' . "\r\n";
+/* $headers .= 'Cc: someone@someplace.com' . "\r\n"; */
+
+mail($to,$subject,$message,$headers);
+?>
