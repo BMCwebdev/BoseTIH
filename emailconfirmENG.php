16a3ee885aaf02386af93e4e5012b4ddae0f2909
Brian <Brian@bmcwebdev.com>
2014-11-02 00:48:26 -0400

    revert

--- emailconfirmENG.php
+++ emailconfirmENG.php
@@ -1,43 +1,48 @@
 <?php
-// multiple recipients
-$to  = 'aidan@example.com' . ', '; // note the comma
-$to .= 'wez@example.com';
+$to = "!!!!!!!!!!!!!!!!!";
+$subject = "Bose &ldquo;Bring It Home!&rdquo; Program – Enrollment Confirmation";
 
-// subject
-$subject = 'Birthday Reminders for August';
-
-// message
-$message = '
+$message = "
 <html>
 <head>
-  <title>Birthday Reminders for August</title>
+    <title>Bose &ldquo;Bring It Home!&rdquo; Program – Enrollment Confirmation</title>
 </head>
+
 <body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Image</td><td>3rd</td><td><img src="http://yourdomain.com/images/example.jpg"></td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
+<div>
+<img src="/assets/images/hero.jpg" alt="hero" style="width:100%;" />
+</div>
+
+    <p>Bose and its promotion partner Promosis, Inc. sent you this message. To ensure delivery to your inbox (not bulk or spam folders), please add <a href="mailto:bringithome@email-hub.com">bringithome@email-hub.com</a> to your address book.</p>
+
+    <p>Dear [first name],<br />
+    <br />
+    Thanks for enrolling in the Bose&#174; &ldquo;<em>Bring It Home!&rdquo;</em> SoundTouch&#8482; systems sales program.</p>
+
+    <p>Log-in to the site to submit each of your sales of a qualifying SoundTouch(TM) system<br />
+    <br />
+    If you qualify for a prize, it will be shipped to the address supplied during program enrollment. If your shipping address has changed since you enrolled, you must let us know the new address.</p>
+
+   <div>**********</div><br />
+    <div>Your enrolled E-mail address is: [email address]</div><br />
+    <div>***********</div>
+    <br />
+    Thanks again for joining our promotion, and good luck!<br />
+    <br />
+    [Official Rules link]<br />
+    <br />
+    Click here to be removed from our email list.</p>
 </body>
 </html>
-';
+";
 
-// To send HTML mail, the Content-type header must be set
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-// Additional headers
-$headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
-$headers .= 'From: Birthday Reminder <birthday@example.com>' . "\r\n";
+// More headers
+$headers .= 'From: <bringithome@email-hub.com>' . "\r\n";
+/* $headers .= 'Cc: someone@someplace.com' . "\r\n"; */
 
-
-// Mail it
-mail($to, $subject, $message, $headers);
+mail($to,$subject,$message,$headers);
 ?>
